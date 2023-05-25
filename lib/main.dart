9cbaecba2891922658442fde1cc0ed3dc43f4010
NithinYesudas <nithinypadickal@gmail.com>
2023-05-25 14:07:49 +0530

    Added chats

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,17 @@ void main() {
         child: const Icon(Icons.message),
       ),
       body: ListView(
-        scrollDirection: Axis.horizontal,
 
         children:[
+         ListTile(
+           leading: CircleAvatar(
 
+             backgroundImage: NetworkImage("https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cG9ydHJhaXR8ZW58MHx8MHx8fDA%3D&w=1000&q=80"),
+           ),
+           title: Text("Nithin",style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
+           subtitle: Text("Hi, How're you pal..!"),
+           trailing: Text("10:00 AM"),
+         )
 
         ]
       )
