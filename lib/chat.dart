a478b3360f35e7478f5bb533aea8108eacab5726
NithinYesudas <nithinypadickal@gmail.com>
2023-05-25 15:51:35 +0530

    Changed the project structure

--- lib/chat.dart
+++ lib/chat.dart
@@ -2,6 +2,8 @@ import 'package:firstproject/subtitle.dart';
 import 'package:firstproject/time.dart';
 import 'package:flutter/material.dart';
 
+import 'name.dart';
+
 class Chat extends StatelessWidget{
   Chat(this.name,this.message,this.time);
   String name;
@@ -15,7 +17,7 @@ class Chat extends StatelessWidget{
 
         backgroundImage: NetworkImage("https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cG9ydHJhaXR8ZW58MHx8MHx8fDA%3D&w=1000&q=80"),
       ),
-      title: Text(name,style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
+      title: Name(name),
       subtitle: Subtitle(message),
       trailing: Time(time)
     );
